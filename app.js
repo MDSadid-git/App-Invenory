6db9011ae5af7f0719496d896bb1c16d61f21f88
Sadid <sfhsadid@gmail.com>
2023-08-10 01:14:56 +0600

    Sadid Product Router Schema done

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
+const {
+  getProducts,
+  createProducts,
+} = require("./controllers/product.controllers");
 
 //middlewares
 app.use(express.json());
@@ -11,8 +15,8 @@ app.get("/", (req, res) => {
   res.send("Route is working! YOYO");
 });
 
-app.get("/api/v1/product");
+app.get("/api/v1/product", getProducts);
 
-app.post("/api/v1/product");
+app.post("/api/v1/product", createProducts);
 
 module.exports = app;
