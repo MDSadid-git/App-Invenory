83aac20dc9b4205d3dbd9ee1e1309874843059ab
Sadid <sfhsadid@gmail.com>
2023-08-10 20:02:00 +0600

    Sadid Servic area new againg update

--- server.js
+++ server.js
@@ -5,9 +5,14 @@ const colors = require("@colors/colors");
 const app = require("./app");
 
 // database connection
-mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
-  console.log(`Database connection is successfull`.red.bold);
-});
+mongoose
+  .connect(process.env.DATABASE_LOCAL)
+  .then(() => {
+    console.log(`Database connection is successfull`.red.bold);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 //server
 const port = process.env.PORT || 5050;
