67e1eccbb218a6418515cca200cb8031ec96e036
Sadid <sfhsadid@gmail.com>
2023-11-13 04:57:10 +0600

    Sadid Brand email & website Schema create

--- models/Brand.js
+++ models/Brand.js
@@ -1,4 +1,5 @@
 const { default: mongoose } = require("mongoose");
+const validator = require("validator");
 
 const brandSchema = mongoose.Schema({
   name: {
@@ -9,4 +10,12 @@ const brandSchema = mongoose.Schema({
     unique: true,
   },
   description: String,
+  email: {
+    type: String,
+    validate: [validator.isEmail, "Please Provide a valid email"],
+  },
+  website: {
+    type: String,
+    validate: [validator.isURL, "Please Provide a valid url"],
+  },
 });
