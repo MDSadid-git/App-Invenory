a8dba328d6772e578c554916dd622b3521c892cd
Sadid <sfhsadid@gmail.com>
2023-11-13 23:08:23 +0600

    Sadid store  Schema done

---
+++ models/Store.js
@@ -0,0 +1,51 @@
+const { default: mongoose } = require("mongoose");
+const validator = require("validator");
+const { ObjectId } = mongoose.Schema.Types;
+
+const storeSchema = mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    require: [true, "Please provide a Store name"],
+    maxLenght: 100,
+    unique: true,
+    lowercase: true,
+  },
+  description: String,
+  email: {
+    type: String,
+    validate: [validator.isEmail, "Please Provide a valid email"],
+    lowercase: true,
+  },
+  website: {
+    type: String,
+    validate: [validator.isURL, "Please Provide a valid url"],
+  },
+  location: String,
+  products: [
+    {
+      type: ObjectId,
+      ref: "Product",
+    },
+  ],
+  suppliers: [
+    {
+      name: String,
+      contanctNumber: String,
+      id: {
+        type: ObjectId,
+        ref: "Supplier",
+      },
+    },
+  ],
+  status: {
+    type: String,
+    enum: ["active", "in-active"],
+    default: "active",
+  },
+
+  timestamps: true,
+});
+
+const Store = mongoose.model("Store", StoreSchema);
+exports = Store;
