74436604dbfd270e471d79d72f67d7c79b38e31b
Sadid <sfhsadid@gmail.com>
2023-11-13 23:05:45 +0600

    Sadid Category  Schema done

---
+++ models/Category.js
@@ -0,0 +1,22 @@
+const { default: mongoose } = require("mongoose");
+const validator = require("validator");
+const { ObjectId } = mongoose.Schema.Types;
+
+const categorySchema = mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: [true, "Please Provide a catoegory name"],
+    lowercase: true,
+    unique: true,
+  },
+  decription: String,
+  imageUrl: {
+    type: String,
+    validate: [validator.isURL, "Please provide a valid URL"],
+  },
+  timestamps: true,
+});
+
+const Category = mongoose.model("Category", categorySchema);
+exports = Category;
