70b8f955830528ae91e608772e9372461ee3868f
Sadid <sfhsadid@gmail.com>
2023-11-14 08:28:23 +0600

    Product Schema new Update

--- models/Product.js
+++ models/Product.js
@@ -1,4 +1,5 @@
 const { default: mongoose } = require("mongoose");
+const { ObjectId } = mongoose.Schema.Types;
 
 //Schema design
 const productSchema = new mongoose.Schema(
@@ -10,47 +11,86 @@ const productSchema = new mongoose.Schema(
       unique: [true, "Name must be unique"],
       minlength: [3, "Name must be at least 3 characters."],
       maxlength: [100, "Name is to large."],
+      lowercase: true,
     },
     description: {
       type: String,
       require: true,
     },
-    price: {
-      type: Number,
-      require: true,
-      min: [0, "Price can't be negative."],
-    },
+    // price: {
+    //   type: Number,
+    //   require: true,
+    //   min: [0, "Price can't be negative."],
+    // },
     unit: {
       type: String,
       require: true,
       enum: {
-        values: ["kg", "liter", "pcs"],
-        message: [true, "Unit value can't be {Value}, must be kg/liter/pcs."],
+        values: ["kg", "liter", "pcs", "bag"],
+        message: [
+          true,
+          "Unit value can't be {Value}, must be kg/liter/pcs/bag.",
+        ],
       },
     },
-    quantity: {
-      type: Number,
-      require: true,
-      min: [0, "Quantity can't be Negative"],
-      validate: {
-        validator: (value) => {
-          const isInterg = Number.isInteger(value);
-          if (isInterg) {
-            return true;
-          } else {
-            return false;
-          }
+    imageURLs: [
+      {
+        type: String,
+        required: true,
+        validate: {
+          validator: (value) => {
+            if (!Array.isArray(value)) {
+              return false;
+            }
+            let isValid = true;
+            value.forEach((url) => {
+              if (!validator.isURL(url)) {
+                isValid = false;
+              }
+            });
+            return isValid;
+          },
         },
       },
-    },
-    stauts: {
+    ],
+    category: {
       type: String,
-      require: true,
-      enum: {
-        values: ["in-stock", "out-of-stock", "discontinued"],
-        message: "Status can't be {value}",
+      required: true,
+    },
+    brand: {
+      name: {
+        type: String,
+        required: true,
+      },
+      id: {
+        type: ObjectId,
+        ref: "Brand",
+        required: true,
       },
     },
+    // quantity: {
+    //   type: Number,
+    //   require: true,
+    //   min: [0, "Quantity can't be Negative"],
+    //   validate: {
+    //     validator: (value) => {
+    //       const isInterg = Number.isInteger(value);
+    //       if (isInterg) {
+    //         return true;
+    //       } else {
+    //         return false;
+    //       }
+    //     },
+    //   },
+    // },
+    // stauts: {
+    //   type: String,
+    //   require: true,
+    //   enum: {
+    //     values: ["in-stock", "out-of-stock", "discontinued"],
+    //     message: "Status can't be {value}",
+    //   },
+    // },
     // createdAt: {
     //   type: Date,
     //   default: Date.now,
